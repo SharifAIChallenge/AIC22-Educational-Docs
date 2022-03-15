99bd374585d1e03c8466be855f21fc54d5ca6f65
mhbahmani <mhbahmani79@gmail.com>
2022-03-15 10:30:45 +0330

    Add flask app

---
+++ Infrastructure/Phase1-Nginx/app/api.py
@@ -0,0 +1,25 @@
+from flask import Flask , request
+import sys #for printing in log console
+
+app = Flask(__name__)
+
+
+@app.route('/static')
+def games():
+    return 'wrong! load /static directory'
+
+@app.route('/mobile')
+def mobile():
+    return 'This is mobile version'
+
+@app.route('/limit')
+def limit():
+    return 'This is limit zone!'
+
+@app.route('/endpoint1')
+def limit():
+    return 'This is endpoint1'
+
+@app.route('/endpoint2')
+def limit():
+    return 'This is endpoint2'
